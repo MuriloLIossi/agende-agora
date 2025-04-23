bb4e4ed2649ce0636d86f0291e3adf8021e4c332
Murilo Lopes <murilo.lopes32@etec.sp.gov.br>
2025-04-23 11:58:15 -0300

    adicionada a tabela para exibir os cadastros

--- app/Http/Controllers/PlaceController.php
+++ app/Http/Controllers/PlaceController.php
@@ -14,33 +14,33 @@ class PlaceController extends Controller
      */
     public function index(){
         $places = Place::all();
-        dd($places);
-        return view('Places/table', ['place => $places']);
+        //dd($places);
+        return view('Places\table', ['places' => $places]);
     }
 
     /**
-     * Mostra o formulário para criação de um novo espaço.
+     * Mostra o formulário para criar um novo espaço.
      *
      * @return \Illuminate\View\View
      */
     public function create(){
-        return view('Places/form');
+        return view('Places\form');
     }
 
     /**
-     * Armazena um novo espaço no banco de dados.
+     * Armazena os dados de um novo espaço no banco de dados.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\RedirectResponse
      */
     public function store(Request $request){
         $place = new Place();
         $place->name = $request->name;
-        $place->description = $request->Description;
+        $place->Description = $request->Description;
         $place->capacity = $request->capacity;
 
         //insert no bd
-        $place->save();
+        $place->save(); 
 
         //retorna ao início
         return redirect("/");
--- resources/views/Places/table.blade.php
+++ resources/views/Places/table.blade.php
@@ -2,19 +2,34 @@
 
 @section('content')
     <div class="container mt-4">
-        <div class="card bg-dark text-light shadow">
-            <div class="card-header bg-primary text-white text-center">
-                <h1 class="fw-bold">Meus espaços 🏙</h1>
-            </div>
-            <div class="card-body">
-
-            </div>
-        </div>
+        <table class="table table-dark table-striped">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Nome</th>
+                    <th>Descrição</th>
+                    <th>Capacidade</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($places as $place)
+                    <tr>
+                        <td>{{$place->id}}</td>
+                        <td>{{$place->name}}</td>
+                        <td>{{$place->Description}}</td>
+                        <td>{{$place->capacity}}</td>
+                        <td>    
+                            <button class="btn btn-secondary">
+                                Editar
+                            </button>
+                            <button class="btn btn-danger">
+                                Excluir
+                            </button>
+                        </td>
+                    </tr>
+                @endforeach 
+            </tbody>
+        </table>
     </div>
-
-    <script>
-        function mostrarAviso() {
-            alert("Espaço salvo com sucesso!");
-        }
-    </script>
 @endsection
